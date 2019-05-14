3c9cb6b244cfce1111533448b74a78e05715ee83
marklubarsky <marklubarsky@gmail.com>
2019-05-13 23:34:48 -0700

    WIP

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -1,5 +1,6 @@
 (function (window, document) {
     var menu = document.getElementById('menu'),
+        testimonial = document.getElementById('testimonials'),
         WINDOW_CHANGE_EVENT = ('onorientationchange' in window) ? 'orientationchange':'resize';
 
     function toggleHorizontal() {
@@ -36,6 +37,47 @@
         toggleMenu();
         e.preventDefault();
     });
+
+    function toggleTestimonial(direction) {
+        //
+        var currentTestimonial = testimonials.querySelector('#current');
+        var newTestimonial = null;
+
+        var first = testimonials.querySelectorAll('p').item(0);
+        var last = testimonials.querySelectorAll('p').item(testimonials.querySelectorAll('.testimonial-list p').length - 1);
+        if (currentTestimonial == null) { currentTestimonial = first }
+
+        if (direction == 'right') {
+            newTestimonial = currentTestimonial.nextElementSibling;
+            if (newTestimonial == null) { newTestimonial = first; }
+        }
+
+        if (direction == 'left') {
+            newTestimonial = currentTestimonial.previousElementSibling;
+            if (newTestimonial == null) { newTestimonial = last; }
+        }
+
+        currentTestimonial.id = '';
+        // currentTestimonial.style.display = 'none';
+        currentTestimonial.classList.toggle('show');
+        //
+        newTestimonial.id = 'current';
+        newTestimonial.classList.toggle('show');
+
+
+
+    }
+
+    document.getElementById('testimonial-right').addEventListener('click', function (e) {
+        setTimeout(function() { toggleTestimonial('right') }, 100);
+        e.preventDefault();
+    });
+
+    document.getElementById('testimonial-left').addEventListener('click', function (e) {
+        setTimeout(function() { toggleTestimonial('left') }, 100);
+        e.preventDefault();
+    });
+
 
     window.addEventListener(WINDOW_CHANGE_EVENT, closeMenu);
 })(this, this.document);
