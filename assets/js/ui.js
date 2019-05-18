c18b286d94439f722e0b14e662467a54d8f67948
marklubarsky <marklubarsky@gmail.com>
2019-05-18 00:48:26 -0700

    WIP

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -1,4 +1,5 @@
 (function (window, document) {
+    // window.onload="switchStories()";
     var menu = document.getElementById('menu'),
         testimonial = document.getElementById('testimonials'),
         WINDOW_CHANGE_EVENT = ('onorientationchange' in window) ? 'orientationchange':'resize';
@@ -68,17 +69,57 @@
 
     }
 
-    document.getElementById('testimonial-right').addEventListener('click', function (e) {
-        setTimeout(function() { toggleTestimonial('right') }, 100);
-        e.preventDefault();
-    });
+    /**
+     * Randomize array element order in-place.
+     * Using Durstenfeld shuffle algorithm.
+     */
+    function shuffleArray(array) {
+        for (var i = array.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
 
-    document.getElementById('testimonial-left').addEventListener('click', function (e) {
-        setTimeout(function() { toggleTestimonial('left') }, 100);
-        e.preventDefault();
-    });
+    function swapElements(obj1, obj2) {
+        obj2.nextSibling === obj1
+            ? obj1.parentNode.insertBefore(obj2, obj1.nextSibling)
+            : obj1.parentNode.insertBefore(obj2, obj1);
+    }
+
+    function switchStories() {
+        var sections =  ['olga','mark'];
+        shuffleArray(sections);
+
+        var el1 = document.getElementById(sections[0]);
+        var el2 =  document.getElementById(sections[1]);
+        // var el3 =  document.getElementById(sections[2]);
+
+
+
+       swapElements(el1, el2);
+       // swapElements(el1, el3);
+    }
+
+    if (document.getElementById('testimonial-right') !== null) {
+        document.getElementById('testimonial-right').addEventListener('click', function (e) {
+            setTimeout(function() { toggleTestimonial('right') }, 100);
+            e.preventDefault();
+        });
+
+        document.getElementById('testimonial-left').addEventListener('click', function (e) {
+            setTimeout(function() { toggleTestimonial('left') }, 100);
+            e.preventDefault();
+        });
+    }
 
 
     window.addEventListener(WINDOW_CHANGE_EVENT, closeMenu);
+    window.addEventListener("load", function(){
+        if (document.getElementById('olga')) {
+            switchStories();
+        }
+    });
 })(this, this.document);
 
