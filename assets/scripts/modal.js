3f42f7ca27f6bcbb70ae28f61a72b60adf307a06
kachanovskyi <vova.kachanovsky@gmail.com>
2019-08-09 18:09:48 +0300

    IE style fixes

--- assets/scripts/modal.js
+++ assets/scripts/modal.js
@@ -4,15 +4,21 @@ const modalButton = document.getElementById('modalButton');
 let forms = document.querySelectorAll(".news-letter-form");
 
 forms.forEach(function (elem) {
-    elem.addEventListener('submit', event => {
-        event.preventDefault();
-        toggleMenu();
-    });
+  elem.addEventListener('submit', event => {
+    if (event.preventDefault) {
+      event.preventDefault();
+      event.stopPropagation();
+    } else {
+      event.returnValue = false;
+      event.stopPropagation();
+    }
+    toggleMenu();
+  });
 });
 
 modalButton.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
-    modal.classList.toggle('active');
-    body.classList.toggle('modal-open');
+  modal.classList.toggle('active');
+  body.classList.toggle('modal-open');
 }
