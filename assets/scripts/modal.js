b2a4e7379856e243b9df13f5c43bb797ba3214e2
marklubarsky <marklubarsky@gmail.com>
2019-08-10 08:07:57 -0700

    Add newsletter integration

--- assets/scripts/modal.js
+++ assets/scripts/modal.js
@@ -12,7 +12,7 @@ forms.forEach(function (elem) {
       event.returnValue = false;
       event.stopPropagation();
     }
-    toggleMenu();
+    submitToAPI(event);
   });
 });
 
@@ -21,4 +21,48 @@ modalButton.addEventListener('click', toggleMenu);
 function toggleMenu() {
   modal.classList.toggle('active');
   body.classList.toggle('modal-open');
+}
+
+
+function submitToAPI(e) {
+    e.preventDefault();
+    var URL = "https://iw7ggnrpwf.execute-api.us-west-2.amazonaws.com/prod";
+
+    if (($("#email-input").val()=="") && ($("#footer-email-input").val()=="")) {
+        alert ("Please enter your email id");
+        return;
+    }
+
+    // var reemail = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,6})?$/;
+    // if (!reemail.test($("#email-input").val())) {
+    //     alert ("Please enter valid email address");
+    //     return;
+    // }
+
+    var email = $("#email-input").val() || $("#footer-email-input").val();
+    var data = {
+        lists:[{id: "1086612997"}],
+        email_addresses: [{email_address: email}]
+    };
+
+    $.ajax({
+        type: "POST",
+        url : URL,
+        dataType: "json",
+        crossDomain: "true",
+        contentType: "application/json;",
+        beforeSend: function(xhr){xhr.setRequestHeader("X-Api-Key","4aJNB1bkZ54rgkdTZGYQX41FQhD5leU490d2QZ9T");},
+        data: JSON.stringify(data),
+
+        success: function (e) {
+            $("#email-input").val("");
+            $("#footer-email-input").val("");
+            toggleMenu();
+        },
+        error: function(xhr,err){
+            if (xhr.responseJSON[0]) {
+                console.log(xhr.responseJSON[0].error_message);
+                alert('Please enter a valid email address.');
+            }
+        }});
 }
